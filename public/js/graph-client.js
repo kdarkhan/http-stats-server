22d458e26d26bd14de38c6b99c867eb2c5eb9f0d
Darkhan Kubigenov <dakubigenov@ebay.com>
2014-07-17 10:41:18 -0700

    added highcharts theme

--- public/js/graph-client.js
+++ public/js/graph-client.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function constructAllGraphs(data) {
+
     console.log(data);
     var showCPU, showMemory;
     var stepCount;
@@ -52,9 +53,15 @@ function constructAllGraphs(data) {
                 data: []
             });
         });
+
+        // get timestamps
+        data.forEach(function(timedata) {
+            timestamps.push(new Date(timedata.timestamp).getTime()); 
+        });
+
         for (var i = 0; i < array.length; i++) {
             var timePoint = data[i];
-            timestamps.push(new Date(timePoint.timestamp));
+            // timestamps.push(new Date(timePoint.timestamp).getTime());
             var steps = timePoint.steps;
             for (var j = 0; j < steps.length; j++) {
                 var stats = steps[j].stats;
@@ -106,7 +113,7 @@ function constructAllGraphs(data) {
             }
         });
     }
-
+/*
     function addRequestPerSecGraphs(reqPerSecData, timestamps, container) {
         return addGraphsToContainer({
             data: reqPerSecData,
@@ -121,6 +128,9 @@ function constructAllGraphs(data) {
         });
     }
 
+*/
+
+/*
     function addResponseTimeGraphs(responseTimeData, timestamps, container) {
         return addGraphsToContainer({
             data: responseTimeData,
@@ -134,7 +144,9 @@ function constructAllGraphs(data) {
             averageTitle: 'Average response time'
         });
     }
+*/
 
+/*
     function addCPUGraphs(cpuUsage, timestamps, container) {
         return addGraphsToContainer({
             data: cpuUsage,
@@ -149,6 +161,10 @@ function constructAllGraphs(data) {
         });
     }
 
+    */
+
+    /*
+
     function addMemoryGraphs(memoryUsageData, timestamps, container) {
         return addGraphsToContainer({
             data: memoryUsageData,
@@ -162,7 +178,62 @@ function constructAllGraphs(data) {
             averageTitle: 'Average memory usage'
         });
     }
+    */
 
+
+    function newAddReqPerSecGraph(data, timestamps) {
+        console.log('data', data);
+        console.log('time', timestamps);
+        var series = [];
+        for (var i = 0; i < data.length; i++) {
+            var concurrencyData = data[i];
+            var seriesData = {
+                name: concurrencyData.name,
+                data: []
+            };
+            for (var j = 0; j < timestamps.length; j++) {
+                seriesData.data.push([timestamps[j], concurrencyData.data[j]]);
+            }
+            series.push(seriesData);
+        }
+
+        $('#reqPerSecGraph').highcharts({
+            chart: {
+                type: 'spline',
+                zoomType: 'xy'
+            },
+            title: {
+                text: 'Requests per second'
+            },
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: { // don't display the dummy year
+                    month: '%e. %b',
+                    year: '%b'
+                },
+                title: {
+                    text: 'Date'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Requests per second'
+                },
+                min: 0
+            },
+            /*
+                        tooltip: {
+                            headerFormat: '<b>{series.name} concurrency</b><br>',
+                            pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+                        },*/
+            credits: {
+                enabled: false
+            },
+            series: series,
+            useUTC: false
+        });
+    }
+/*
     function addGraphsToContainer(options) {
         var data = options.data;
         var container = options.container || 'body';
@@ -190,7 +261,9 @@ function constructAllGraphs(data) {
         return classList;
 
     }
+*/
 
+/*
     function getGenericGraph(options) {
         var title = options.title || 'Graph';
         var type = options.type || 'line';
@@ -200,7 +273,7 @@ function constructAllGraphs(data) {
         var series = options.series || [];
         return {
             chart: {
-                type: type
+                type: type,
             },
             title: {
                 text: title
@@ -231,7 +304,7 @@ function constructAllGraphs(data) {
             series: series
         };
     }
-
+*/
     function addFilters(concurrencyClasses) {
         var hiddenClasses = [];
 
@@ -331,24 +404,262 @@ function constructAllGraphs(data) {
         addAveragesToData(parsedData);
         $(function() {
             var concurrencyClasses = [];
-            concurrencyClasses.push(addRequestPerSecGraphs(parsedData.reqPerSecData, parsedData.timestamps));
-            concurrencyClasses.push(addResponseTimeGraphs(parsedData.responseTime, parsedData.timestamps));
+            //concurrencyClasses.push(addRequestPerSecGraphs(parsedData.reqPerSecData, parsedData.timestamps));
+            //concurrencyClasses.push(addResponseTimeGraphs(parsedData.responseTime, parsedData.timestamps));
 
             if (showCPU) {
-                concurrencyClasses.push(addCPUGraphs(parsedData.cpuData, parsedData.timestamps));
+                //concurrencyClasses.push(addCPUGraphs(parsedData.cpuData, parsedData.timestamps));
             }
             if (showMemory) {
-                concurrencyClasses.push(addMemoryGraphs(parsedData.memoryData, parsedData.timestamps));
+                //concurrencyClasses.push(addMemoryGraphs(parsedData.memoryData, parsedData.timestamps));
             }
             //addConcurrencyFilter(concurrencyClasses);
             //addGraphTypeFilter();
             //window.multiselect();
-            addFilters(concurrencyClasses);
+
+            //addFilters(concurrencyClasses);
+
+            newAddReqPerSecGraph(parsedData.reqPerSecData, parsedData.timestamps);
         });
     }
 }
 
-define(['jquery'], function(jquery) {
+define(['jquery', 'highcharts'], function(jquery, highcharts) {
+
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+
+
+    /**
+     * Dark theme for Highcharts JS
+     * @author Torstein Honsi
+     */
+
+    // Load the fonts
+    /*
+    Highcharts.createElement('link', {
+        href: 'http://fonts.googleapis.com/css?family=Unica+One',
+        rel: 'stylesheet',
+        type: 'text/css'
+    }, null, document.getElementsByTagName('head')[0]);
+*/
+
+    Highcharts.theme = {
+        colors: ["#2b908f", "#90ee7e", "#f45b5b", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
+            "#55BF3B", "#DF5353", "#7798BF", "#aaeeee"
+        ],
+        chart: {
+            backgroundColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 1,
+                    y2: 1
+                },
+                stops: [
+                    [0, '#2a2a2b'],
+                    [1, '#3e3e40']
+                ]
+            },
+            style: {
+                fontFamily: "'Unica One', sans-serif"
+            },
+            plotBorderColor: '#606063'
+        },
+        title: {
+            style: {
+                color: '#E0E0E3',
+                textTransform: 'uppercase',
+                fontSize: '20px'
+            }
+        },
+        subtitle: {
+            style: {
+                color: '#E0E0E3',
+                textTransform: 'uppercase'
+            }
+        },
+        xAxis: {
+            gridLineColor: '#707073',
+            labels: {
+                style: {
+                    color: '#E0E0E3'
+                }
+            },
+            lineColor: '#707073',
+            minorGridLineColor: '#505053',
+            tickColor: '#707073',
+            title: {
+                style: {
+                    color: '#A0A0A3'
+
+                }
+            }
+        },
+        yAxis: {
+            gridLineColor: '#707073',
+            labels: {
+                style: {
+                    color: '#E0E0E3'
+                }
+            },
+            lineColor: '#707073',
+            minorGridLineColor: '#505053',
+            tickColor: '#707073',
+            tickWidth: 1,
+            title: {
+                style: {
+                    color: '#A0A0A3'
+                }
+            }
+        },
+        tooltip: {
+            backgroundColor: 'rgba(0, 0, 0, 0.85)',
+            style: {
+                color: '#F0F0F0'
+            }
+        },
+        plotOptions: {
+            series: {
+                dataLabels: {
+                    color: '#B0B0B3'
+                },
+                marker: {
+                    lineColor: '#333'
+                }
+            },
+            boxplot: {
+                fillColor: '#505053'
+            },
+            candlestick: {
+                lineColor: 'white'
+            },
+            errorbar: {
+                color: 'white'
+            }
+        },
+        legend: {
+            itemStyle: {
+                color: '#E0E0E3'
+            },
+            itemHoverStyle: {
+                color: '#FFF'
+            },
+            itemHiddenStyle: {
+                color: '#606063'
+            }
+        },
+        credits: {
+            style: {
+                color: '#666'
+            }
+        },
+        labels: {
+            style: {
+                color: '#707073'
+            }
+        },
+
+        drilldown: {
+            activeAxisLabelStyle: {
+                color: '#F0F0F3'
+            },
+            activeDataLabelStyle: {
+                color: '#F0F0F3'
+            }
+        },
+
+        navigation: {
+            buttonOptions: {
+                symbolStroke: '#DDDDDD',
+                theme: {
+                    fill: '#505053'
+                }
+            }
+        },
+
+        // scroll charts
+        rangeSelector: {
+            buttonTheme: {
+                fill: '#505053',
+                stroke: '#000000',
+                style: {
+                    color: '#CCC'
+                },
+                states: {
+                    hover: {
+                        fill: '#707073',
+                        stroke: '#000000',
+                        style: {
+                            color: 'white'
+                        }
+                    },
+                    select: {
+                        fill: '#000003',
+                        stroke: '#000000',
+                        style: {
+                            color: 'white'
+                        }
+                    }
+                }
+            },
+            inputBoxBorderColor: '#505053',
+            inputStyle: {
+                backgroundColor: '#333',
+                color: 'silver'
+            },
+            labelStyle: {
+                color: 'silver'
+            }
+        },
+
+        navigator: {
+            handles: {
+                backgroundColor: '#666',
+                borderColor: '#AAA'
+            },
+            outlineColor: '#CCC',
+            maskFill: 'rgba(255,255,255,0.1)',
+            series: {
+                color: '#7798BF',
+                lineColor: '#A6C7ED'
+            },
+            xAxis: {
+                gridLineColor: '#505053'
+            }
+        },
+
+        scrollbar: {
+            barBackgroundColor: '#808083',
+            barBorderColor: '#808083',
+            buttonArrowColor: '#CCC',
+            buttonBackgroundColor: '#606063',
+            buttonBorderColor: '#606063',
+            rifleColor: '#FFF',
+            trackBackgroundColor: '#404043',
+            trackBorderColor: '#404043'
+        },
+
+        // special colors for some of the
+        legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+        background2: '#505053',
+        dataLabelsColor: '#B0B0B3',
+        textColor: '#C0C0C0',
+        contrastTextColor: '#F0F0F3',
+        maskColor: 'rgba(255,255,255,0.3)'
+    };
+
+    // Apply the theme
+    Highcharts.setOptions(Highcharts.theme);
+
+
+
+
+
     return {
         constructAllGraphs: constructAllGraphs
     };
