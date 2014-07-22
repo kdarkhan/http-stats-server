a89453fd896aa6e847c32afe05902e4c33f8a2c0
Darkhan Kubigenov <dakubigenov@ebay.com>
2014-07-22 11:36:53 -0700

    define separate entry points for homepage and project_view page

--- public/js/app.js
+++ public/js/app.js
@@ -5,25 +5,71 @@ require.config({
     paths: {
         jquery: '//code.jquery.com/jquery-1.11.0.min',
         jqueryui: '//code.jquery.com/ui/1.11.0/jquery-ui.min',
-        moment: 'moment',
-        hcoptions: 'hc-options',
-        graphclient: 'graph-client',
-        main: 'main'
-    },
-    shim: {
-        'highcharts': {
-            'exports': 'Highcharts',
-            'deps': ['jquery']
-        },
     }
 });
 
 
-require(['main'], function(main) {
+require(['jquery'], function(jquery) {
 
+    function parseParameters() {
+        return {
+            _csrf: $('[name="_csrf"]').val(),
+            options: {
+                name: $('#projectName').val(),
+                url: $('#url').val(),
+                beginConcurrency: $('#beginConcurrency').val(),
+                peakConcurrency: $('#peakConcurrency').val(),
+                endConcurrency: $('#endConcurrency').val(),
+                concurrencyIncrement: $('#concurrencyInc').val(),
+                concurrencyDecrement: $('#concurrencyDec').val(),
+                stepRequests: $('#stepRequests').val(),
+                delay: $('#stepDelay').val(),
+                warmup: $('#warmupEnabled').val()
+            }
+        };
+    }
+
+    function validateInput(parameters) {
+        var urlRegex = new RegExp(/^(ht|f)tps?:\/\/[a-z0-9-\.]+/);
+        if (urlRegex.test(parameters.options.url)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    function createProjectListener() {
+        console.log('create project clicked');
+        var userInput = parseParameters();
+        console.log(userInput);
+
+        if (validateInput(userInput)) {
+            $('.statOptions input,.statOptions button').prop('disabled', true);
+
+            $.ajax({
+                url: 'create_new',
+                type: 'POST',
+                data: userInput,
+                success: function(data) {
+                    if (data && data.status === 'Success') {
+                        $('#current_projects ul').append('<li><a href="' + userInput.options.name + '/">' +
+                            userInput.options.name + '</a>');
+                    } else {
+                        window.alert('Could not create project:\n' + JSON.stringify(data));
+                    }
+                    $('.statOptions input,.statOptions button').prop('disabled', false);
+                },
+                error: function(jqXHR) {
+                    window.alert('Could not create project:\n' + jqXHR.responseText);
+                    $('.statOptions input,.statOptions button').prop('disabled', false);
+                }
+            });
+        }
+    }
     var app = {
         initialize: function() {
             // Your code here
+            $('#create_project').click(createProjectListener);
         }
     };
 
---
+++ public/js/project_view.js
@@ -0,0 +1,94 @@
+'use strict';
+
+
+require.config({
+    paths: {
+        jquery: '//code.jquery.com/jquery-1.11.0.min',
+        jqueryui: '//code.jquery.com/ui/1.11.0/jquery-ui.min',
+        moment: 'moment',
+        hcoptions: 'hc-options',
+        graphclient: 'graph-client'
+    },
+    shim: {
+        'highcharts': {
+            'exports': 'Highcharts',
+            'deps': ['jquery']
+        },
+    }
+});
+
+
+require(['graphclient', 'jquery', 'jqueryui'], function(graphClient) {
+
+
+    function startTest() {
+        var csrfToken = $('[name="_csrf"]').val();
+        var tryCount = 0;
+
+        function pollingStatus() {
+            $.ajax({
+                url: '/get_status',
+                success: function(data) {
+                    if (data && data.testRunning === true) {
+                        tryCount++;
+                        $('#currentStatus').text(data.projectName + ' is running');
+                        // poll every 1 sec 10 times, then poll every 10 sec
+                        setTimeout(pollingStatus, tryCount < 10 ? 1000 : 10000);
+                    } else {
+                        // test is complete, enable the button
+                        $('#currentStatus').text('');
+                        $('#startTest').attr('disabled', false);
+                    }
+                },
+                error: function(jqXHR) {
+                    $('#startTest').attr('disabled', false);
+                    $('#currentStatus').text(jqXHR.responseText);
+                    console.log('jqxhr is ', jqXHR);
+                    // window.alert('error occurred:\n', jqXHR.responseText);
+                }
+            });
+        }
+
+        $('#startTest').attr('disabled', true);
+        $.ajax({
+            url: 'start_test',
+            type: 'POST',
+            data: {
+                _csrf: csrfToken
+            },
+            success: function(data) {
+                if (data && data.status === 'Success') {
+                    console.log('test started');
+                    setTimeout(pollingStatus, 1000);
+                } else {
+                    $('#startTest').attr('disabled', false);
+                    console.log('could not start test');
+                }
+            },
+            error: function(jqXHR) {
+                $('#startTest').attr('disable', false);
+                $('#currentStatus').text(jqXHR.responseText);
+                window.alert('error occurred', jqXHR.responseText);
+            }
+        });
+    }
+
+
+
+    var app = {
+        initialize: function() {
+            // initializeDatepickers();
+            $('#startTest').click(startTest);
+            $('#tabs').tabs({
+                hactivate: function() {
+                    $(window).resize();
+                }
+            });
+            graphClient.constructAllGraphs(window.resultData);
+        }
+    };
+
+
+    app.initialize();
+
+});
